d094e0a7b8f5064bbed4b2a7ff54be64b82d1db1
fuchigta <fuchigta@alpha.co.jp>
2024-12-10 22:32:40 +0900

    本番ビルド時だけベース名を設定するようにした

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   // Config options...
+  basename: process.env.NODE_ENV == "production" ? "/tech-news" : "/",
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
 } satisfies Config;
